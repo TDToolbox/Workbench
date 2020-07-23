6e565325dd6474f36efedfcf5f5707b087608aa5
DisabledMallis <mallisiscool@gmail.com>
2020-07-22 21:30:05 -0400

    my shit

--- Workbench/Workbench/Windows/MallisTesting.xaml.cs
+++ Workbench/Workbench/Windows/MallisTesting.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BTD_Backend;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,7 @@ namespace Workbench
         public MallisTesting()
         {
             InitializeComponent();
+            Log.Output("how");
         }
     }
 }
