a43dc598a9a42e1416ad33c9b5a545c0fca2f238
DisabledMallis <mallisiscool@gmail.com>
2020-07-25 19:47:35 -0400

    Start reading jet for tree view

--- Workbench/Workbench/Windows/MallisTesting.xaml.cs
+++ Workbench/Workbench/Windows/MallisTesting.xaml.cs
@@ -24,12 +24,35 @@ namespace Workbench
     /// </summary>
     public partial class MallisTesting : Window
     {
+        private Zip jet;
 
         public MallisTesting()
         {
             InitializeComponent();
         }
 
+        public void openJet(string path)
+        {
+            openJet(new Zip(path));
+        }
+        public void openJet(Zip jet)
+        {
+            this.jet = jet;
+            List<string> entries = jet.GetEntries(Zip.EntryType.Directories, default, System.IO.SearchOption.TopDirectoryOnly);
+            entries.ForEach((entry) =>
+            {
+                TreeViewItem item = new TreeViewItem();
+                item.Header = entry;
+                item.Expanded += TreeViewItem_Expanded;
+                //item.Items.Add(new TreeViewItem());
+                JetView.Items.Add(item);
+            });
+            /*List<string> entries = jet.GetEntries(Zip.EntryType.Directories, default, System.IO.SearchOption.TopDirectoryOnly);
+            entries.ForEach((entry) =>
+            {
+                JetView.Items.Add(recurseFile(entry, jet));
+            });*/
+        }
 
         private void MallisTestingWindow_Loaded(object sender, RoutedEventArgs e)
         {
@@ -45,17 +68,45 @@ namespace Workbench
             LinedTextBox_UC linedTextBox = new LinedTextBox_UC();
             linedTextBox.TextEditor.Text = text;
             //ContentPanel.Children.Add(linedTextBox);*/
+            openJet("BTD5.jet");
         }
 
         private void TreeViewItem_Expanded(object sender, RoutedEventArgs e)
         {
             TreeViewItem source = (TreeViewItem)e.Source;
-            TreeViewItem newItem = new TreeViewItem();
+            TreeViewItem current = source;
+            string path = "";
+            while(current.Parent is TreeViewItem || current.Parent is TreeView)
+            {
+                if(current.Parent is TreeViewItem)
+                {
+                    path = current.Header + path;
+                    current = (TreeViewItem)current.Parent;
+                }
+                else
+                {
+                    path = current.Header + path;
+                    break;
+                }
+            }
+            //path = path.Substring(0, path.Length - 1);
+            Log.Output(path);
+            if(current.Items.Count < 2)
+            {
+                List<string> entries = jet.GetEntries(Zip.EntryType.Directories, path, System.IO.SearchOption.TopDirectoryOnly);
+                entries.ForEach((entry) =>
+                {
+                    TreeViewItem item = new TreeViewItem();
+                    item.Header = entry.Replace(path, "");
+                    current.Items.Add(item);
+                });
+            }
+            /*TreeViewItem newItem = new TreeViewItem();
             newItem.Header = "Gameing";
             Label l = new Label();
             l.Content = "Gameing";
             newItem.Items.Add(l);
-            source.Items.Add(newItem);
+            source.Items.Add(newItem);*/
         }
 
         private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
