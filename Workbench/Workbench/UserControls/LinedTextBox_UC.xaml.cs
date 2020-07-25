8a199272165fc67bbe4b1c674b6aa85c6d6bee4a
DisabledMallis <mallisiscool@gmail.com>
2020-07-25 05:58:15 -0400

    Syntax highlighting, Refactor TabTextEditor to TabEditor

--- Workbench/Workbench/UserControls/LinedTextBox_UC.xaml.cs
+++ Workbench/Workbench/UserControls/LinedTextBox_UC.xaml.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using BTD_Backend;
+using ICSharpCode.AvalonEdit;
+using ICSharpCode.AvalonEdit.Highlighting;
+using ICSharpCode.AvalonEdit.Highlighting.Xshd;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +18,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace Workbench.UserControls
 {
@@ -20,9 +27,26 @@ namespace Workbench.UserControls
     /// </summary>
     public partial class LinedTextBox_UC : UserControl
     {
+        public static Stream GenerateStreamFromString(string s)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
         public LinedTextBox_UC()
         {
             InitializeComponent();
+            string json = Properties.Resources.BJson;
+            using(Stream s = GenerateStreamFromString(json))
+            {
+                using (XmlTextReader reader = new XmlTextReader(s))
+                {
+                    TextEditor.SyntaxHighlighting = HighlightingLoader.Load(reader, HighlightingManager.Instance);
+                }
+            }
         }
     }
 }
